8ee69594c862055e578374a22f1877a597a9d139
niko9083 <niko9083@edu.aarhustech.dk>
2021-03-05 13:59:09 +0100

    Cleaned up the code a lil'

--- Classes.py
+++ Classes.py
@@ -1,11 +1,12 @@
 import pygame
 
+
 class svarBox:
     def __init__(self, x, y, screen):
         self.x = x
         self.y = y
         self.width = 100
-        self.height =69
+        self.height = 69
         self.color = (255, 255, 255)
         self.theScreen = screen
 
@@ -13,9 +14,7 @@ class svarBox:
         pygame.draw.rect(self.theScreen, self.color, pygame.Rect(self.x, self.y, self.width, self.height))
 
     def isMouseOver(self, mouseX, mouseY):
-        if mouseX>self.x and mouseX< self.x + self.width and mouseY>self.y and mouseY<self.y + self.height:
+        if mouseX > self.x and mouseX < self.x + self.width and mouseY > self.y and mouseY < self.y + self.height:
             return True
         else:
             return False
-
-
--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import pygame
 from Classes import svarBox
+
 svarBoxes = []
-import time
 
 pygame.init()
 
-windowWidth = 1080
-windowHeight = 760
+windowWidth = 640
+windowHeight = 480
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((windowWidth, windowHeight))
 
@@ -14,9 +14,10 @@ titleFont = pygame.font.SysFont("Arial", 100, True)
 font = pygame.font.SysFont("Arial", 50)
 sponsorFont = pygame.font.SysFont("Arial", 20)
 
-svarBoxes.append(svarBox(500,500,screen))
+svarBoxes.append(svarBox(windowWidth / 2, windowHeight / 2, screen))
 
-def mainMenu():
+
+def main():
     global done
     done = False
     levels = False
@@ -31,42 +32,42 @@ def mainMenu():
 
             mouseX, mouseY = pygame.mouse.get_pos()
             mouseText = sponsorFont.render(f"{mouseX} ; {mouseY}", False, (255, 255, 255))
-            screen.blit(mouseText, (0, windowHeight-20))
+            screen.blit(mouseText, (0, windowHeight - 20))
 
             titleText = titleFont.render("DuoPython", False, (255, 255, 255))
 
             if pygame.mouse.get_pressed()[0]:
                 for box in svarBoxes:
                     if box.isMouseOver(mouseX, mouseY):
-                        box.x=mouseX-box.width/2
-                        box.y=mouseY-box.height/2
+                        box.x = mouseX - box.width / 2
+                        box.y = mouseY - box.height / 2
 
             # Level select button
             if mouseX <= 245 and 210 >= mouseY >= 150:
                 levelSelectText = font.render(" Level Select ", False, (0, 0, 0), (255, 255, 255))
-                if (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     levels = not levels
             else:
                 levelSelectText = font.render(" Level Select ", False, (255, 255, 255))
 
             if levels:
                 # If statement button
-                if 300 <= mouseX <= 545 and 150 <= mouseY <= 200:
-                    ifs = font.render(" If statemens ", False, (0, 0, 0), (255, 255, 255))
+                if 300 <= mouseX <= 580 and 150 <= mouseY <= 200:
+                    ifs = font.render(" > If statemens ", False, (0, 0, 0), (255, 255, 255))
                 else:
-                    ifs = font.render(" If statemens ", False, (255, 255, 255))
+                    ifs = font.render(" > If statemens ", False, (255, 255, 255))
 
                 # For loops button
-                if 300 <= mouseX <= 495 and 225 <= mouseY <= 275:
-                    fors = font.render(" For loops ", False, (0, 0, 0), (255, 255, 255))
+                if 300 <= mouseX <= 530 and 225 <= mouseY <= 275:
+                    fors = font.render(" > For loops ", False, (0, 0, 0), (255, 255, 255))
                 else:
-                    fors = font.render(" For loops ", False, (255, 255, 255))
+                    fors = font.render(" > For loops ", False, (255, 255, 255))
 
                 # While loops button
-                if 300 <= mouseX <= 535 and 300 <= mouseY <= 350:
-                    whiles = font.render(" While loops ", False, (0, 0, 0), (255, 255, 255))
+                if 300 <= mouseX <= 570 and 300 <= mouseY <= 350:
+                    whiles = font.render(" > While loops ", False, (0, 0, 0), (255, 255, 255))
                 else:
-                    whiles = font.render(" While loops ", False, (255, 255, 255))
+                    whiles = font.render(" > While loops ", False, (255, 255, 255))
 
                 screen.blit(ifs, (300, 150))
                 screen.blit(fors, (300, 225))
@@ -84,9 +85,10 @@ def mainMenu():
             screen.blit(titleText, (0, 0))
             screen.blit(levelSelectText, (0, 150))
             screen.blit(quitText, (0, 225))
-            for ting in svarBoxes: #mouse box
-                ting.draw()        #mouse box
+            for ting in svarBoxes:  # mouse box
+                ting.draw()  # mouse box
             pygame.display.flip()
             clock.tick(60)
 
-mainMenu()
+
+main()
